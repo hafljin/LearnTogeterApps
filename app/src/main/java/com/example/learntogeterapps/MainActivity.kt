aa9efdd2eef414cf524c2d4c0d052db44734022c
Mic <michelnativedeveloper@gmail.com>
2024-04-20 15:13:44 +0900

    もうすぐ完成だけど意外にサイズ調整とかに苦戦だけど一旦コミット

--- app/src/main/java/com/example/learntogeterapps/MainActivity.kt
+++ app/src/main/java/com/example/learntogeterapps/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.example.learntogeterapps
 
+import android.health.connect.datatypes.units.Length
 import android.os.Bundle
+import android.webkit.WebSettings.TextSize
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.learntogeterapps.ui.theme.LearnTogeterAppsTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,7 +38,20 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    TopScreen()
+                    TopScreen(
+                        messageTitle = "Jetpack Compose tutorial",
+                        messageTop = "Jetpack Compose tutorial\n" +
+                                "Jetpack Compose is a modern toolkit for building native Android UI. " +
+                                "Compose simplifies and accelerates UI development on Android with less code," +
+                                " powerful tools, and intuitive Kotlin APIs.",
+                        messageDisp = "In this tutorial, you build a simple UI component with declarative functions." +
+                                " You call Compose functions to say what elements you want and the Compose compiler does the rest. " +
+                                "Compose is built around Composable functions. These functions let you define your app\\'s " +
+                                "UI programmatically because they let you describe how it should look and provide data dependencies," +
+                                " rather than focus on the process of the UI\\'s construction, " +
+                                "such as initializing an element and then attaching it to a parent. To create a Composable function, " +
+                                "you add the @Composable annotation to the function name."
+                    )
                 }
             }
         }
@@ -38,19 +62,38 @@ class MainActivity : ComponentActivity() {
 fun TopScreen(
     messageTitle: String,
     messageTop: String,
-    modiferDiscrip: Modifier = Modifier
+    messageDisp: String,
+    modifier: Modifier = Modifier
 ) {
     val topImage = painterResource(R.drawable.bg_compose_background)
-    Column () {
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        //top image
         Image(
-            painter = topImage ,
+            painter = topImage,
             contentDescription = "null"
         )
-        //タイトル
-        Text(
+        Spacer(modifier = Modifier.height(8.dp))
+        // title
+        Text( // todo後で解決
             messageTitle,
-            modifier = Modifier
-                .padding(8.dp)
+            fontSize = 20.sp,
+            Modifier.padding(8.dp)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        // body
+        Text(
+            messageTop,
+            Modifier.padding(8.dp)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        // discriptions
+        Text(
+            messageDisp.take(20000),// maxLength使えないのであり得ない文字数を入れる
+            Modifier.padding(8.dp)
         )
     }
 }
@@ -59,6 +102,19 @@ fun TopScreen(
 @Composable
 fun GreetingPreview() {
     LearnTogeterAppsTheme {
-        TopScreen()
+        TopScreen(
+            messageTitle = "Jetpack Compose tutorial",
+            messageTop = "Jetpack Compose tutorial\n" +
+                    "Jetpack Compose is a modern toolkit for building native Android UI. " +
+                    "Compose simplifies and accelerates UI development on Android with less code," +
+                    " powerful tools, and intuitive Kotlin APIs.",
+            messageDisp = "In this tutorial, you build a simple UI component with declarative functions." +
+                    " You call Compose functions to say what elements you want and the Compose compiler does the rest. " +
+                    "Compose is built around Composable functions. These functions let you define your app\\'s " +
+                    "UI programmatically because they let you describe how it should look and provide data dependencies," +
+                    " rather than focus on the process of the UI\\'s construction, " +
+                    "such as initializing an element and then attaching it to a parent. To create a Composable function, " +
+                    "you add the @Composable annotation to the function name.",
+        )
     }
 }
