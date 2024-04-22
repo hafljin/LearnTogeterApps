c1388bcec09d2b6f6a64d9549003c2166f6dd73e
Mic <michelnativedeveloper@gmail.com>
2024-04-23 00:37:18 +0900

    タスクマネージャー完成

--- app/src/main/java/com/example/learntogeterapps/MainActivity.kt
+++ app/src/main/java/com/example/learntogeterapps/MainActivity.kt
@@ -1,28 +1,31 @@
 package com.example.learntogeterapps
 
-import android.health.connect.datatypes.units.Length
 import android.os.Bundle
-import android.webkit.WebSettings.TextSize
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -38,19 +41,25 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    TopScreen(
-                        messageTitle = "Jetpack Compose tutorial",
-                        messageTop = "Jetpack Compose tutorial\n" +
-                                "Jetpack Compose is a modern toolkit for building native Android UI. " +
-                                "Compose simplifies and accelerates UI development on Android with less code," +
-                                " powerful tools, and intuitive Kotlin APIs.",
-                        messageDisp = "In this tutorial, you build a simple UI component with declarative functions." +
-                                " You call Compose functions to say what elements you want and the Compose compiler does the rest. " +
-                                "Compose is built around Composable functions. These functions let you define your app\\'s " +
-                                "UI programmatically because they let you describe how it should look and provide data dependencies," +
-                                " rather than focus on the process of the UI\\'s construction, " +
-                                "such as initializing an element and then attaching it to a parent. To create a Composable function, " +
-                                "you add the @Composable annotation to the function name."
+//                    TopScreen(
+//                        messageTitle = "Jetpack Compose tutorial",
+//                        messageTop = "Jetpack Compose tutorial\n" +
+//                                "Jetpack Compose is a modern toolkit for building native Android UI. " +
+//                                "Compose simplifies and accelerates UI development on Android with less code," +
+//                                " powerful tools, and intuitive Kotlin APIs.",
+//                        messageDisp = "In this tutorial, you build a simple UI component with declarative functions." +
+//                                " You call Compose functions to say what elements you want and the Compose compiler does the rest. " +
+//                                "Compose is built around Composable functions. These functions let you define your app\\'s " +
+//                                "UI programmatically because they let you describe how it should look and provide data dependencies," +
+//                                " rather than focus on the process of the UI\\'s construction, " +
+//                                "such as initializing an element and then attaching it to a parent. To create a Composable function, " +
+//                                "you add the @Composable annotation to the function name."
+//                    )
+                    TaskManager(
+                        iconSize = 48,
+                        color = Color.Green,
+                        messageCenter = "All tasks completed",
+                        messageBody = "Nice work!"
                     )
                 }
             }
@@ -78,9 +87,8 @@ fun TopScreen(
         )
         Spacer(modifier = Modifier.height(8.dp))
         // title
-        Text( // todo後で解決
+        Text(
             messageTitle,
-            fontSize = 20.sp,
             Modifier.padding(8.dp)
         )
         Spacer(modifier = Modifier.height(8.dp))
@@ -94,6 +102,39 @@ fun TopScreen(
         Text(
             messageDisp.take(20000),// maxLength使えないのであり得ない文字数を入れる
             Modifier.padding(8.dp)
+        )
+    }
+}
+
+@Composable
+fun TaskManager(
+    iconSize:Int,
+    color: Color,
+    messageCenter: String,
+    messageBody: String
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+    ) {
+        Icon(
+            imageVector = Icons.Default.Check,
+            contentDescription = "Check Icon",
+            modifier = Modifier.size(iconSize.dp),
+            tint = color
+
+        )
+        Text(
+            messageCenter,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(24.dp,24.dp,0.dp,8.dp)
+        )
+        Text(
+            messageBody,
+            fontSize = 16.sp,
         )
     }
 }
@@ -116,5 +157,18 @@ fun GreetingPreview() {
                     "such as initializing an element and then attaching it to a parent. To create a Composable function, " +
                     "you add the @Composable annotation to the function name.",
         )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun IconPreview(){
+    LearnTogeterAppsTheme {
+      TaskManager(
+          iconSize = 48,
+          color = Color.Green,
+          messageCenter = "All tasks completed",
+          messageBody = "Nice work!"
+      )
     }
 }
